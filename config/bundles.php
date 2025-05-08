67c257474a690101b610f21c21b26ecab1e42341
Vladislav Levchenko <clernovmail@gmail.com>
2025-05-08 19:07:36 +0300

    add sonata admin, configured article admin page

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,13 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Sonata\Doctrine\Bridge\Symfony\SonataDoctrineBundle::class => ['all' => true],
+    Sonata\Twig\Bridge\Symfony\SonataTwigBundle::class => ['all' => true],
+    Sonata\Form\Bridge\Symfony\SonataFormBundle::class => ['all' => true],
+    Sonata\Exporter\Bridge\Symfony\SonataExporterBundle::class => ['all' => true],
+    Sonata\BlockBundle\SonataBlockBundle::class => ['all' => true],
+    Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
+    Sonata\AdminBundle\SonataAdminBundle::class => ['all' => true],
+    Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle::class => ['all' => true],
 ];
---
+++ src/Admin/ArticleAdmin.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+final class ArticleAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $form): void
+    {
+        $form->add('title', TextType::class);
+        $form->add('author', TextType::class);
+        $form->add('text', TextType::class);
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagrid): void
+    {
+        $datagrid->add('title');
+        $datagrid->add('author');
+    }
+
+    protected function configureListFields(ListMapper $list): void
+    {
+        $list->addIdentifier('title');
+        $list->addIdentifier('author');
+    }
+
+    protected function configureShowFields(ShowMapper $show): void
+    {
+        $show->add('title');
+        $show->add('author');
+        $show->add('text');
+    }
+}
